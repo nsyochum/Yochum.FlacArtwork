de78222c473f1786a8f5c0dded05d5f27016c6d9
nsyochum <nsyochum@live.com>
2025-11-15 23:44:44 -0700

    updated to user Serilog

--- Yochum.FlacArtwork/Program.cs
+++ Yochum.FlacArtwork/Program.cs
@@ -1,4 +1,6 @@
-﻿using TagLib;
+﻿using Serilog;
+using System.Runtime.CompilerServices;
+using TagLib;
 
 namespace Yochum.FlacArtwork;
 
@@ -6,16 +8,17 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        InitLogger();
         if (args.Length == 0)
         {
-            Console.WriteLine("Usage: app <directory>");
+            Log.Error("Usage: Yochum.FlacArtwork <directory>");
             return;
         }
 
         var rootDir = args[0];
         if (!Directory.Exists(rootDir))
         {
-            Console.WriteLine("Directory not found.");
+            Log.Error("Directory not found.");
             return;
         }
 
@@ -34,13 +37,17 @@ public class Program
 
             if (flacFiles.Length != 0 && imageFiles.Length != 0)
             {
-                Console.WriteLine($"Processing folder: {subDir}");
+                Log.Information($"Processing folder: {subDir}");
                 var imagePath = imageFiles.First(); // pick first image
 
                 foreach (var flac in flacFiles)
                 {
                     AddArtwork(flac, imagePath);
                 }
+            }
+            else
+            {
+                Log.Information($"Skipping dir: {subDir}, {flacFiles.Length} FLAC files, {imageFiles.Length} Image files");
             }
 
             // Recurse into deeper subdirectories
@@ -56,11 +63,29 @@ public class Program
             var picture = new Picture(imagePath);
             file.Tag.Pictures = [picture];
             file.Save();
-            Console.WriteLine($"Updated artwork for: {flacPath}");
+            Log.Information($"Updated artwork for: {flacPath}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error processing {flacPath}: {ex.Message}");
+            Log.Error($"Error processing {flacPath}: {ex.Message}");
         }
+    }
+
+    private static void InitLogger()
+    {
+        var now = DateTimeOffset.UtcNow;
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    Path.Combine(
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        "Logs",
+                        $"Log_.log"
+                    ),
+                    rollingInterval: RollingInterval.Hour,
+                    outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message}{NewLine}{Exception}"
+                )
+                .CreateLogger();
     }
 }
